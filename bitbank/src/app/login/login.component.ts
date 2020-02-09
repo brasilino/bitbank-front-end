19cc2da5363c56f856177a97495e8aa74ed820db
Alan Silva Brasilino <alan_brasilino@icloud.com>
2020-02-09 01:59:06 -0300

    Adiciona autenticação do usuário

--- bitbank/src/app/login/login.component.ts
+++ bitbank/src/app/login/login.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-login',
@@ -8,17 +17,23 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
-  form: FormGroup = new FormGroup({
-    username: new FormControl(''),
-    password: new FormControl(''),
-  });
-
+  form: FormGroup;
+  matcher: MyErrorStateMatcher;
   error: string | null;
 
   constructor() { }
 
   ngOnInit() {
-    this.error = 'Usuário inválido.';
+    this.form = new FormGroup({
+      cpf: new FormControl('', [
+        Validators.required,
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+      ]),
+    });
+
+    this.matcher = new MyErrorStateMatcher();
   }
 
   submit() {
@@ -27,6 +42,8 @@ export class LoginComponent implements OnInit {
 
     if (this.form.valid) {
       console.log(this.form.value);
+    } else {
+      console.log(this.form);
     }
   }
 
